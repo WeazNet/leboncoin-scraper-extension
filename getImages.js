38d847609cea41767fb62d5b7dfc8758a36c451f
WeazNet <weazlife@gmail.com>
2023-06-03 19:18:49 +0200

    fix config

--- getImages.js
+++ getImages.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll(".slick-slide img");
 let adImages = [];
-for (let i = 0; i < images.length/2; i++) {
+for (let i = 0; i < (images.length/2)-1; i++) {
   adImages[i] = images[i].src;
 }
 
