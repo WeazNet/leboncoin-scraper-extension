f7c4e70797b4b64fcbf09c0e1aeda269d11deff9
Vlad Alexandru Tanasov <52748732+WeazNet@users.noreply.github.com>
2024-12-26 10:06:02 +0100

    first commit v2

--- getContent.js
+++ getContent.js
@@ -1,63 +1,135 @@
-const adTitle = document.getElementsByClassName("_1hnil _35DXM _1-TTU")[0].textContent;
-const adPrice = document.querySelector('[data-qa-id="adview_price"]').getElementsByTagName("span")[0].textContent.slice(0, -2).replace(/\s/g, '');
-let date_a = document.querySelector('[data-qa-id="adview_date"]').textContent;
-let dateParts = date_a.split(" à ");
-const adDate = dateParts[0].split("/").reverse().join("-") + "T" + dateParts[1] + ":00";
-const adBrand = document.querySelector('[data-qa-id="criteria_item_u_car_brand"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adModel = document.querySelector('[data-qa-id="criteria_item_u_car_model"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adYear = document.querySelector('[data-qa-id="criteria_item_regdate"]').getElementsByTagName("span")[0].textContent.toLowerCase();
+const adTitle =
+  document.querySelector('[data-qa-id="adview_title"]')?.textContent ?? "";
+const adPrice =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="adview_price"]')
+      ?.getElementsByTagName("p")[0]
+      ?.textContent.slice(0, -2)
+      .replace(/\s/g, "")
+  ) || -1;
+const adBrand =
+  document
+    .querySelector('[data-qa-id="criteria_item_u_car_brand"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adModel =
+  document
+    .querySelector('[data-qa-id="criteria_item_u_car_model"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adYear =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_regdate"]')
+      ?.getElementsByTagName("p")[1]
+      ?.textContent.toLowerCase()
+  ) || -1;
+const adIssuanceDate =
+  document
+    .querySelector('[data-qa-id="criteria_item_issuance_date"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adMileage =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_mileage"]')
+      ?.getElementsByTagName("p")[1]
+      ?.textContent.slice(0, -3)
+  ) || 0;
+const adFuel =
+  document
+    .querySelector('[data-qa-id="criteria_item_fuel"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adGearbox =
+  document
+    .querySelector('[data-qa-id="criteria_item_gearbox"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adType =
+  document
+    .querySelector('[data-qa-id="criteria_item_vehicle_type"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adColor =
+  document
+    .querySelector('[data-qa-id="criteria_item_vehicule_color"]')
+    ?.getElementsByTagName("p")[1]
+    ?.textContent.toLowerCase() ?? "";
+const adNbDoors =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_doors"]')
+      ?.getElementsByTagName("p")[1]?.textContent
+  ) || 0;
+const adNbSeats =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_seats"]')
+      ?.getElementsByTagName("p")[1]
+      ?.textContent.toLowerCase()
+  ) || 0;
+const adHorsepower =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_horsepower"]')
+      ?.getElementsByTagName("p")[1]
+      ?.textContent.slice(0, -3)
+  ) || 0;
+const adHorsepowerDIN =
+  parseInt(
+    document
+      .querySelector('[data-qa-id="criteria_item_horse_power_din"]')
+      ?.getElementsByTagName("p")[1]
+      ?.textContent.slice(0, -3)
+  ) || 0;
 
-const adIssuanceDate = document.querySelector('[data-qa-id="criteria_item_issuance_date"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adMileage = document.querySelector('[data-qa-id="criteria_item_mileage"]').getElementsByTagName("span")[0].textContent.slice(0, -3);
-const adFuel = document.querySelector('[data-qa-id="criteria_item_fuel"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adGearbox = document.querySelector('[data-qa-id="criteria_item_gearbox"]').getElementsByTagName("span")[0].textContent.toLowerCase();
+const adSaddlery =
+  document
+    .querySelector('[data-qa-id="criteria_item_vehicle_upholstery"]')
+    ?.getElementsByTagName("p")[1]?.textContent ?? "";
 
-const adType = document.querySelector('[data-qa-id="criteria_item_vehicle_type"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adColor = document.querySelector('[data-qa-id="criteria_item_vehicule_color"]').getElementsByTagName("span")[0].textContent.toLowerCase();
+// Extraction de la description de l'annonce
+const adDescription =
+  document
+    .querySelector('[data-qa-id="adview_description_container"]')
+    ?.getElementsByTagName("p")[0]?.textContent ?? "";
 
-const adNbDoors = document.querySelector('[data-qa-id="criteria_item_doors"]').getElementsByTagName("span")[0].textContent;
-const adNbSeats = document.querySelector('[data-qa-id="criteria_item_seats"]').getElementsByTagName("span")[0].textContent.toLowerCase();
-const adHorsepower = document.querySelector('[data-qa-id="criteria_item_horsepower"]').getElementsByTagName("span")[0].textContent.slice(0, -3);
-const adHorsepowerDIN = document.querySelector('[data-qa-id="criteria_item_horse_power_din"]').getElementsByTagName("span")[0].textContent.slice(0, -3);
-const adDescription = document.querySelector('[data-qa-id="adview_description_container"]').getElementsByTagName("p")[0].textContent;
-
-let adOptions = []; 
-
-let checkImages = document.querySelector('[data-qa-id="adview_spotlight_container"]').getElementsByTagName("button")[0];
-checkImages.click();
-
-
+let adOptions = [];
 const regex = /option(?:s)?\s*([\s\S]*?)etc/i;
-
 const match = regex.exec(adDescription);
-if(match)
-{
+if (match) {
   const optionsTexte = match[1].trim();
-  adOptions = optionsTexte.split('\n');
+  adOptions = optionsTexte.split("\n");
   adOptions.shift();
 }
 
+let adImages = [];
+const imgContainer = document.querySelectorAll(".slick-slide");
+for (let i = 1; i < imgContainer.length / 2; i++) {
+  adImages.push(imgContainer[i].querySelector("img").src);
+}
 
 const data = {
   title: adTitle,
-  price: parseInt(adPrice),
-  date: new Date(adDate),
+  price: adPrice,
   brand: adBrand,
   model: adModel,
-  year: parseInt(adYear),
+  year: adYear,
   issuanceDate: adIssuanceDate,
-  mileage: parseInt(adMileage),
+  mileage: adMileage,
   fuel: adFuel,
   gearbox: adGearbox,
+  saddlery: adSaddlery,
   type: adType,
   color: adColor,
-  nbDoors: parseInt(adNbDoors),
-  nbSeats: parseInt(adNbSeats),
-  horsepower: parseInt(adHorsepower),
-  horsepowerDIN: parseInt(adHorsepowerDIN),
+  nbDoors: adNbDoors,
+  nbSeats: adNbSeats,
+  horsepower: adHorsepower,
+  horsepowerDIN: adHorsepowerDIN,
   description: adDescription,
-  images:[],
-  options:adOptions,
+  images: adImages,
+  options: adOptions,
 };
-
 data;
--- popup.js
+++ popup.js
@@ -1,36 +1,303 @@
 function downloadJSON(data) {
-    const json = JSON.stringify(data);
-    const blob = new Blob([json], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    let title = data["title"];
-    title = title.replaceAll(' ', '-');
-    title += "-data.json";
-    a.download = title;
-    a.href = url;
-    a.click();
+  const json = JSON.stringify(data);
+  const blob = new Blob([json], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  let title = data["title"];
+  title = title.replaceAll(" ", "-");
+  title += "-data.json";
+  a.download = title;
+  a.href = url;
+  a.click();
+}
+
+function storeCredentials(username, password) {
+  const expirationTime = new Date().getTime() + 9999;
+  const credentials = { username, password, expirationTime };
+  localStorage.setItem("credentials", JSON.stringify(credentials));
+}
+
+function storeUrl(url) {
+  localStorage.setItem("url", url);
+}
+
+function getStoredCredentials() {
+  const credentials = JSON.parse(localStorage.getItem("credentials"));
+  if (credentials) {
+    return credentials;
   }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('download-button').addEventListener('click', function() {
+  localStorage.removeItem("credentials");
+  return null;
+}
+
+function storeUrl(url) {
+  localStorage.setItem("url", url);
+}
+
+function getStoredUrl() {
+  return localStorage.getItem("url");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const storedCredentials = getStoredCredentials();
+  const storedUrl = getStoredUrl();
+
+  if (storedCredentials) {
+    document.getElementById("username").value = storedCredentials.username;
+    document.getElementById("password").value = storedCredentials.password;
+    document.getElementById("connexion-div").classList.add("hidden");
+    document.getElementById("sending-div").classList.add("visible");
+
+    if (storedUrl) {
+      document.getElementById("url").value = storedUrl;
+    }
+  }
+
+  document
+    .getElementById("connect-button")
+    .addEventListener("click", function () {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+
+      if (username && password) {
+        storeCredentials(username, password);
+        document.getElementById("error").textContent = "";
+        document.getElementById("connexion-div").classList.add("hidden");
+        document.getElementById("sending-div").classList.add("visible");
+      } else {
+        document.getElementById("error").textContent =
+          "identifiant et mot de passe requis.";
+      }
+    });
+
+  document.getElementById("send-button").addEventListener("click", function () {
+    const url = document.getElementById("url").value;
+    if (url) {
+      storeUrl(url);
+      document.getElementById("error").textContent =
+        "Processing de l'url " + url;
+
+      fetch(url + "/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: document.getElementById("username").value,
+          password: document.getElementById("password").value,
+        }),
+      })
+        .then((response) => {
+          if (response.status != 200) {
+            localStorage.removeItem("credentials");
+            document.getElementById("error").textContent =
+              "Identifiants invalides. Session expirée.";
+          } else {
+            response.json().then((data) => {
+              if (data && data.message) {
+                localStorage.setItem("bearerToken", data.message);
+
+                document.getElementById("error").textContent =
+                  "Token obtenu " + data.message;
+
+                loadScript("navigator.js")
+                  .then(() => loadScript("getContent.js"))
+                  .then(() => {
+                    browser.tabs
+                      .executeScript({ file: "navigator.js" })
+                      .then(() => {
+                        browser.tabs
+                          .executeScript({ file: "getContent.js" })
+                          .then((results) => {
+                            const data = results[0];
+                            document.getElementById("error").textContent =
+                              "Session ouverte avec succès, démarrage de l'envoi.";
+
+                            fetch(url + "/api/admin/cars/save", {
+                              method: "POST",
+                              headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${localStorage.getItem(
+                                  "bearerToken"
+                                )}`,
+                              },
+                              body: JSON.stringify(data),
+                            })
+                              .then((response) => response.json())
+                              .then((result) => {
+                                if (result.status === 400) {
+                                  document.getElementById("error").textContent =
+                                    result.message;
+                                } else {
+                                  document.getElementById("error").textContent =
+                                    "Envoi vers l'API : OK";
+                                }
+                              });
+                          })
+                          .catch((err) => {
+                            document.getElementById("error").textContent =
+                              "Erreur d'exécution des scripts : " + err;
+                          });
+                      })
+                      .catch((err) => {
+                        document.getElementById("error").textContent =
+                          "Erreur lors du chargement des scripts : " + err;
+                      });
+                  });
+              }
+            });
+          }
+        })
+        .catch((err) => {
+          document.getElementById("error").textContent =
+            "Erreur réseau: " + err;
+        });
+    } else {
+      document.getElementById("error").textContent = "URL requise.";
+    }
+  });
+
+  function loadScript(scriptName) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = scriptName;
+      script.onload = () => resolve();
+      script.onerror = (err) =>
+        reject(`Impossible de charger ${scriptName}: ${err}`);
+      document.head.appendChild(script);
+    });
+  }
+
+  document
+    .getElementById("download-button")
+    .addEventListener("click", function () {
       let data = {};
-      browser.tabs.executeScript({ file: 'openDescription.js'})
-      .then(() => {
-        browser.tabs.executeScript({ file: 'getContent.js' })
-        .then((results) => {
-          data = results[0];
-          browser.tabs.executeScript({ file: 'getImages.js' })
-          .then((images) => {
-            data.images = images[0];
+      browser.tabs.executeScript({ file: "navigator.js" }).then(() => {
+        browser.tabs
+          .executeScript({ file: "getContent.js" })
+          .then((results) => {
+            data = results[0];
             downloadJSON(data);
           })
           .catch((err) => {
             document.getElementById("error").textContent = err;
           });
+      });
+    });
+});
+
+function storeCredentials(username, password) {
+  const expirationTime = new Date().getTime() + 9999;
+  const credentials = { username, password, expirationTime };
+  localStorage.setItem("credentials", JSON.stringify(credentials));
+}
+
+function getStoredCredentials() {
+  const credentials = JSON.parse(localStorage.getItem("credentials"));
+  if (credentials) {
+    return credentials;
+  }
+  localStorage.removeItem("credentials");
+  return null;
+}
+
+function storeUrl(url) {
+  localStorage.setItem("url", url);
+}
+
+function getStoredUrl() {
+  return localStorage.getItem("url");
+}
+
+function storeToken(token) {
+  const now = new Date().getTime();
+  const tokenData = { token, timestamp: now };
+  localStorage.setItem("bearerTokenData", JSON.stringify(tokenData));
+}
+
+function getStoredToken() {
+  const tokenData = JSON.parse(localStorage.getItem("bearerTokenData"));
+  if (tokenData) {
+    return tokenData;
+  }
+  return null;
+}
+
+function isTokenValid() {
+  const tokenData = getStoredToken();
+  if (tokenData) {
+    const now = new Date().getTime();
+    const twoHoursInMillis = 2 * 60 * 60 * 1000;
+    return now - tokenData.timestamp < twoHoursInMillis;
+  }
+  return false;
+}
+
+function downloadJSON(data) {
+  const json = JSON.stringify(data);
+  const blob = new Blob([json], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  let title = data["title"] || "data";
+  title = title.replaceAll(" ", "-");
+  title += "-data.json";
+  a.download = title;
+  a.href = url;
+  a.click();
+}
+
+function loadScript(scriptName) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = scriptName;
+    script.onload = () => resolve();
+    script.onerror = (err) =>
+      reject(`Impossible de charger ${scriptName}: ${err}`);
+    document.head.appendChild(script);
+  });
+}
+
+function sendDataToApi(url) {
+  loadScript("navigator.js")
+    .then(() => loadScript("getContent.js"))
+    .then(() => {
+      browser.tabs
+        .executeScript({ file: "navigator.js" })
+        .then(() => {
+          browser.tabs
+            .executeScript({ file: "getContent.js" })
+            .then((results) => {
+              const data = results[0];
+              document.getElementById("error").textContent =
+                "Session ouverte avec succès";
+
+              fetch(url + "/api/admin/cars/save", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${getStoredToken().token}`,
+                },
+                body: JSON.stringify(data),
+              })
+                .then((response) => response.json())
+                .then((result) => {
+                  if (result.status !== 200) {
+                    document.getElementById("error").textContent =
+                      result.message;
+                  } else {
+                    document.getElementById("error").textContent =
+                      "Envoi vers l'API : OK";
+                  }
+                });
+            })
+            .catch((err) => {
+              document.getElementById("error").textContent =
+                "Erreur d'exécution des scripts : " + err;
+            });
         })
         .catch((err) => {
-          document.getElementById("error").textContent = err;
+          document.getElementById("error").textContent =
+            "Erreur lors du chargement des scripts : " + err;
         });
-      });   
     });
-  });
+}
